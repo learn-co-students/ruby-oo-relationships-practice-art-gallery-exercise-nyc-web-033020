cade1b79351219d08fe981f30cde858703cf851d
mishyjari <mishyjari@outlook.com>
2020-04-05 12:54:11 -0400

    all deliverables met

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,12 +1,24 @@
 class Painting
+  @@all = []
 
-  attr_reader :title, :price
+  attr_reader :title, :price, :artist, :gallery
 
   @@all = []
 
-  def initialize(title, price)
+  def initialize(title, price, artist, gallery)
     @title = title
     @price = price
+    @artist = artist
+    @gallery = gallery
+    Painting.all << self
+  end
+  
+  def self.all 
+    @@all
+  end
+
+  def self.total_price
+    Painting.all.map{ |painting| painting.price }.sum
   end
 
 end
