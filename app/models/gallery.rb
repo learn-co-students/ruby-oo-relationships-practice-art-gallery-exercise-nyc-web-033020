c0408b332946260a469ea8c73863608e753165db
Liz00000000000 <Elizabeth.Michael.Karen@gmail.com>
2020-04-06 19:35:40 -0400

    updated artgallery

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,10 +1,37 @@
 class Gallery
 
   attr_reader :name, :city
+  @@all = [ ]
 
   def initialize(name, city)
     @name = name
     @city = city
+    Gallery.all << self 
   end
+
+  def self.all
+    @@all
+  end 
+
+  def paintings #yay
+  #Returns an array of all paintings in a gallery
+   Painting.all.select do |pic|
+    pic.gallery == self 
+   end 
+  end 
+ 
+  def artists #cool 
+  #Returns an array of all artists that have a painting in a gallery
+  paintings.map {|pic| pic.artist }
+  end 
+  
+  def artist_names #yay
+  #Returns an array of the names of all artists that have a painting in a gallery
+   artists.map{ |artist| artist.name }
+  end 
+
+  def most_expensive_painting
+   #Returns an instance of the most expensive painting in a gallery
+  end 
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -2,6 +2,23 @@ require_relative '../config/environment.rb'
 
 
 
+g = Artist.new("Greg", 100)
+e = Artist.new("Liz", "2 years")
+k = Artist.new("Kenji", "one month")
+
+b = Gallery.new("Art", "NY,NY")
+c = Gallery.new("SMFA", "Boston")
+d = Gallery.new("ugh", "Chicago")
+i = Gallery.new("ok", "LA")
+h = Gallery.new("hmmm", "Austin, TX")
+
+p1 = Painting.new("Selfie part 1", 80, b, g)
+p2 = Painting.new("Exploring Space and Time", 75, c, e)
+p3 = Painting.new("The beauty of an orange cat", 5, d, k)
+p4 = Painting.new("Selfie part 2: exploration of the devine", 100000000 , i, g)
+p5 = Painting.new("Untitiled", 5, h, e)
+p6 = Painting.new("Untitled 1", 8000000000, b, k)
+p7 = Painting.new("Untitled 2", 25000000000, c, g)
 
 binding.pry
 
